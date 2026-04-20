3125856107c634144e6cd34f351d0c2fd20b9c52
Paweł Jarosz <jarosz.pawel@outlook.com>
2026-04-20 17:42:31 +0200

    Add logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use log::{error, warn, info, debug};
+use simplelog::*;
+
 fn main() {
     println!("Hello, world!");
+    CombinedLogger::init(
+        vec![
+            TermLogger::new(LevelFilter::Trace, Config::default(), TerminalMode::Mixed, ColorChoice::Auto),
+        ]
+    ).unwrap();
+    error!("Bright red error");
+    info!("This only appears in the log file");
+    warn!("This level is currently not enabled for any logger");
+    debug!("This level is currently not enabled for any logger");
 }
